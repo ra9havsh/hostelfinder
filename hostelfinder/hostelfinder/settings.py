56fd8a1d83bfc5f3ea8812d61de7581819b736ff
ra9havsh <ra9havsh@gmail.com>
2019-04-23 14:01:42 +0545

    commit

--- hostelfinder/hostelfinder/settings.py
+++ hostelfinder/hostelfinder/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hostelAdmin.apps.HosteladminConfig',
+    'webpage.apps.WebpageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
