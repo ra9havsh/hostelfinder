c88162c561af2510b3a41c548ae82817c8c1e7bb
ra9havsh <ra9havsh@gmail.com>
2019-05-01 00:04:27 +0545

    commit

--- hostelfinder/webpage/urls.py
+++ hostelfinder/webpage/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.conf.urls import url
 
-app_name = 'homepage'
+app_name = 'webpage'
 
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
+    url(r'^register/', views.register_view, name='register'),
 ]
--- hostelfinder/webpage/views.py
+++ hostelfinder/webpage/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request,'webpage/hostel_detail.html')
+    return render(request,'webpage/home_page.html')
+
+def register_view(request):
+    return render(request,'webpage/register.html')
