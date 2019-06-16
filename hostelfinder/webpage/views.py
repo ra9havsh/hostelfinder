49c4b3705817269e33890b0a0f62a3533d74eddf
raghshre <ra9havsh@gmail.com>
2019-06-16 23:33:11 +0545

    commit

--- hostelfinder/webpage/views.py
+++ hostelfinder/webpage/views.py
@@ -6,6 +6,7 @@ from hostelAdmin.models import Hostel, Room, Location, Fee, Image
 from hostelAdmin.forms import HostelForm, RoomForm, FeeForm, RoomDetailForm, ImageForm
 from webpage.models import User, HostelOwner, Student, Rating
 import json
+import math
 
 def popular_hostel():
     rating = Rating.objects.values_list('hostel','avg').order_by('-avg').distinct()[:9]
@@ -16,6 +17,135 @@ def popular_hostel():
     avg_hostel = Hostel.objects.filter(id__in=hostel)
     # print(avg_hostel)
     return avg_hostel
+
+def evaluate_cosine_similarity(user,similar_user):
+    numerator = 0
+    user_square_sum = 0
+    similar_user_square_sum = 0
+
+    for i in range(len(user)-1):
+        numerator = numerator + (user[i]*similar_user[i])
+        user_square_sum = user_square_sum + (user[i]*user[i])
+        similar_user_square_sum = similar_user_square_sum + (user[i]*user[i])
+
+    denominator = math.sqrt(user_square_sum) + math.sqrt(similar_user_square_sum)
+    similarity = numerator/denominator
+    return similarity
+
+def predict_user_item_rating(user,similar_user,cosine_similarity):
+    denominator = 0
+    predictate_rating = []
+
+    similar_user_average = []
+    user_average = 0
+
+    for i in range(len(cosine_similarity)):
+        denominator = denominator + cosine_similarity[i]
+
+    for i in range(len(similar_user)):
+        average = 0;
+        total = 0;
+        for j in range(len(similar_user[0])):
+            total = total + similar_user[i][j]
+        average = total/len(similar_user[0])
+        similar_user_average.append(average)
+
+    total = 0
+    for i in range(len(user)):
+        total = total + user[i]
+    user_average  = total/len(user)
+
+    for j in range(len(similar_user[0])):
+        numerator = 0
+        for i in range(len(similar_user)):
+            numerator = numerator + ((similar_user[i][j]-similar_user_average[i])*cosine_similarity[i])
+        predict = user_average + (numerator/denominator)
+        predictate_rating.append(predict)
+
+    return predictate_rating
+
+def similar_hostel(user_id):
+    user_rated_hostel = []
+    similar_rating_user_id = []
+    similar_user_rated_hostels = []
+    student = Student.objects.get(user_id=user_id)
+    rating = Rating.objects.filter(user=student.user_id)
+
+    if student.gender == 'M':
+        hostel_type = 'B'
+    else:
+        hostel_type = 'G'
+
+    #get the hostels rated by active user
+    for rating in rating:
+        user_rated_hostel.append(rating.hostel)
+
+    #get the similar users who rated the hostels rated by active user
+    similar_rating = Rating.objects.filter(hostel__in=user_rated_hostel,user__student__gender=student.gender).order_by('user_id').exclude(user_id=user_id)
+
+    #prepare the list of similar rating users and the hostels they rated
+    for s in similar_rating:
+        similar_rating_user_id.append(s.user_id)
+
+        for h in Rating.objects.filter(user_id=s.user_id,hostel__hostel_type='B'):
+            similar_user_rated_hostels.append(h.hostel_id)
+
+    # removing duplicates
+    similar_rating_user_id = list(dict.fromkeys(similar_rating_user_id))
+    similar_user_rated_hostels = list(dict.fromkeys(similar_user_rated_hostels))
+    similar_rating_user_id.sort()
+    similar_user_rated_hostels.sort()
+
+    #create user-item matrix for both active user and similar users
+    similar_user =  [[0] * len(similar_user_rated_hostels) for i in range(len(similar_rating_user_id))]
+    user = []
+
+    for i,similar_user_id in enumerate(similar_rating_user_id):
+        for j,hostel_id in enumerate(similar_user_rated_hostels):
+            if Rating.objects.filter(user_id=similar_user_id,hostel_id=hostel_id).exists():
+                r = Rating.objects.values('rating').get(user_id=similar_user_id,hostel_id=hostel_id)
+                rate = r['rating']
+            else:
+               rate = 0
+            similar_user[i][j] = rate
+
+
+    for j in similar_user_rated_hostels:
+        if Rating.objects.filter(user_id=user_id, hostel_id=j).exists():
+            r = Rating.objects.values('rating').get(user_id=user_id, hostel_id=j)
+            rate = r['rating']
+        else:
+            rate = 0
+        user.append(rate)
+
+    #calculating similarity through cosine similarity
+    cosine_similarity = []
+
+    for i in range(len(similar_rating_user_id)):
+        similarity = evaluate_cosine_similarity(user,similar_user[i])
+        cosine_similarity.append(similarity)
+
+    #predicting the rating for the each item of active user
+    predictate_rating = predict_user_item_rating(user,similar_user,cosine_similarity)
+
+    #now finding the top predictate hostel rating
+    predictate_similar_hostel = []
+    hostels = []
+    for j, hostel_id in enumerate(similar_user_rated_hostels):
+        predictate_similar_hostel.append((hostel_id,predictate_rating[j]))
+    predictate_similar_hostel.sort(key=lambda x  : x[1],reverse=True)
+
+    for p in predictate_similar_hostel:
+        hostels.append(p[0])
+
+    hostel = []
+    for h in hostels:
+        hostel.append( Hostel.objects.get(id=h))
+
+    for h in hostel:
+        print(h)
+
+    return hostel[:12]
 
 def log_in_session(request):
     if 'user_id' in request.session:
@@ -396,6 +526,7 @@ def user_student(request,user_id):
         args["username"] = user.user_name
         args["user_id"] = user_id
         args['avg_hostels'] = popular_hostel()
+        args['similar_hostels'] = similar_hostel(user_id)
         return render(request, 'webpage/home_page.html',args)
     else:
         raise Http404('Page not found with user Id : ' + user_id)
